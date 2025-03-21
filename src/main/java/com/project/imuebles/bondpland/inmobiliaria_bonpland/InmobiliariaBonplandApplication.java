a66be7eebbc1700e8c1bd55f0979881e278f5e3b
kevinvittor <kevinvittor579@gmail.com>
2025-03-20 21:41:50 -0300

    run

---
+++ src/main/java/com/project/imuebles/bondpland/inmobiliaria_bonpland/InmobiliariaBonplandApplication.java
@@ -0,0 +1,13 @@
+package com.project.imuebles.bondpland.inmobiliaria_bonpland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmobiliariaBonplandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmobiliariaBonplandApplication.class, args);
+	}
+
+}
