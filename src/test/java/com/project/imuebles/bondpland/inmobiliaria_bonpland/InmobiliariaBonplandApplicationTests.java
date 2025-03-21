8080730f9ba7a18ddbad87c6a068707d7da0c1ad
kevinvittor <kevinvittor579@gmail.com>
2025-03-20 21:43:37 -0300

    primer commit

---
+++ src/test/java/com/project/imuebles/bondpland/inmobiliaria_bonpland/InmobiliariaBonplandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.imuebles.bondpland.inmobiliaria_bonpland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobiliariaBonplandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
