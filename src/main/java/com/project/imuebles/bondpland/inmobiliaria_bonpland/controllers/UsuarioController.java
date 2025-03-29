d42d8c98cc0601f4b2219fb859fcd2964a4df790
kevinvittor <kevinvittor579@gmail.com>
2025-03-29 13:41:59 -0300

    Agregue nuevos metodos para la interaccion con la pagina del cliente

--- src/main/java/com/project/imuebles/bondpland/inmobiliaria_bonpland/controllers/UsuarioController.java
+++ src/main/java/com/project/imuebles/bondpland/inmobiliaria_bonpland/controllers/UsuarioController.java
@@ -1,13 +1,32 @@
 package com.project.imuebles.bondpland.inmobiliaria_bonpland.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.imuebles.bondpland.inmobiliaria_bonpland.services.InmuebleService;
+
 
 @Controller
 @RequestMapping("/cliente")
 public class UsuarioController {
+
+    @Autowired
+    private InmuebleService inmuebleService;
+
     // Implementar el filtrado a los usuarios.
-    public String filtradoInmobiliaria(){
-        return "filtrado";
+    @GetMapping
+    public String filtradoInmobiliaria(Model model){
+        model.addAttribute("listadoInmueble", inmuebleService.findAll());
+        model.addAttribute("listadoInmueblePorPais", inmuebleService.findAllPais());
+        model.addAttribute("listadoInmueblePorCiudad", inmuebleService.findAllCiudad());
+        model.addAttribute("precioMinimo", inmuebleService.precioMinimo());
+        model.addAttribute("precioMaximo", inmuebleService.precioMaximo());
+        model.addAttribute("cantidadAmbientes", inmuebleService.cantidadAmbientes());
+        model.addAttribute("metrosCuadradosMinimo", inmuebleService.metrosCuadradosMinimo());
+        model.addAttribute("metrosCuadradosMaximo", inmuebleService.metrosCuadradosMaximo());
+        return "filtros/filterList";
     }
 }
